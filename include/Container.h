821ddfbfcbce30630c061f636821a19fc60a4f56
shua1090 <shynn.lawrence@gmail.com>
2021-10-19 21:57:18 -0700

    Single Linked List

--- include/Container.h
+++ include/Container.h
@@ -4,28 +4,28 @@
 
 #ifndef STDLIB_CONTAINER_H
 #define STDLIB_CONTAINER_H
+
 #include <iostream>
 
 #define container_size unsigned long long
 
-enum ReturnCode
-{
+enum ReturnCode {
     OK = 0,  // OK (there is a reason for it to be 0)
     KO = -1,      // An error occurred, wait for next message
     FATAL = -2    // A critical error occurred, reboot
 };
 
 
-template <class T>
-class ArrayList{
+template<class T>
+class ArrayList {
 protected:
     container_size capacity{};
     container_size size{};
-    T * beginning;
+    T *beginning;
 
-    void doubleSize(){
-        T * temp = (T *) calloc(this->capacity*2, sizeof(T));
-        for (int i = 0; i < this->size; i++){
+    void doubleSize() {
+        T *temp = (T *) calloc(this->capacity * 2, sizeof(T));
+        for (int i = 0; i < this->size; i++) {
             temp[i] = this->beginning[i];
         }
         this->capacity *= 2;
@@ -34,16 +34,16 @@ protected:
     };
 
 public:
-    ArrayList<T>(){
-        beginning = (T * )calloc(10, sizeof(T));
+    ArrayList<T>() {
+        beginning = (T *) calloc(10, sizeof(T));
         this->capacity = 10;
         this->size = 0;
     };
 
-    void add(T value){
-        if (this->size + 1 > this->capacity){
-            T * temp = (T *) calloc(this->capacity*2, sizeof(T));
-            for (int i = 0; i < this->size; i++){
+    void add(T value) {
+        if (this->size + 1 > this->capacity) {
+            T *temp = (T *) calloc(this->capacity * 2, sizeof(T));
+            for (int i = 0; i < this->size; i++) {
                 temp[i] = this->beginning[i];
             }
             temp[this->size] = value;
@@ -57,23 +57,23 @@ public:
         }
     }
 
-    T get(container_size index){
-        if (index >= this->size || index < 0){
+    T get(container_size index) {
+        if (index >= this->size || index < 0) {
             return 0;
         } else {
             return this->beginning[index];
         }
     }
 
-    ReturnCode insert(container_size index, T value){
-        if (index > this->size || index < 0){return FATAL;}
+    ReturnCode insert(container_size index, T value) {
+        if (index > this->size || index < 0) { return FATAL; }
         else {
 
-            if (this->size + 1 > this->capacity){
+            if (this->size + 1 > this->capacity) {
                 this->doubleSize();
             }
-            for (container_size val = this->size; val >= index; val--){
-                this->beginning[val+1] = this->beginning[val];
+            for (container_size val = this->size; val >= index; val--) {
+                this->beginning[val + 1] = this->beginning[val];
             }
             this->size++;
             this->beginning[index] = value;
@@ -81,12 +81,12 @@ public:
         }
     }
 
-    T remove(container_size index){
-        if (index > this->size || index < 0){return FATAL;}
+    T remove(container_size index) {
+        if (index >= this->size || index < 0) { return FATAL; }
         else {
             T value = this->beginning[index];
-            for (container_size a = index + 1; a < this->size; a++){
-                this->beginning[a-1] = this->beginning[a];
+            for (container_size a = index + 1; a < this->size; a++) {
+                this->beginning[a - 1] = this->beginning[a];
             }
             this->beginning[this->size] = 0;
             this->size--;
@@ -95,22 +95,112 @@ public:
         }
     }
 
-    container_size length(){
+    void enqueue(T value) {
+        this->add(value);
+    }
+
+    T deque() {
+        return this->remove(this->size - 1);
+    }
+
+    T pop() {
+        return this->remove(0);
+    }
+
+    void push(T value) {
+        this->add(value);
+    }
+
+    container_size length() {
         return this->size;
     }
 
-    ~ArrayList(){
+    ~ArrayList() {
         free(this->beginning);
     }
 };
 
-template <class T>
-class Stack : private ArrayList<T>{
+template<class T>
+struct ListNode {
+    T value;
+    ListNode<T> *next;
+};
+
+template<class T>
+class LinkedList_s {
+private:
+    container_size size = 0;
+    ListNode<T> *head = nullptr;
+    ListNode<T> *tail = nullptr;
 public:
-    Stack<T>(): ArrayList<T>(){};
-    T pop(){
+    LinkedList_s() {};
+
+    ~LinkedList_s() {
+        ListNode<T> *current = head;
+        while (true) {
+            ListNode<T> *next = current->next;
+            if (next == nullptr) return;
+            else {
+                delete current;
+            }
+        }
+    }
+
+    void add(T value) {
+        if (head == nullptr) {
+            head = new ListNode<T>{value};
+            tail = new ListNode<T>{value};
+            size++;
+        } else {
+            ListNode<T> *current;
+            current = new ListNode<T>{value};
+            tail->next = current;
+            tail = current;
+            size++;
+        }
+    };
+
+    T get(container_size index) {
+        if (index >= this->size || index < 0) {
+            printf("Index Out of Bounds Error");
+            return NULL;
+        }
+        int i = 0;
+        ListNode<T> *result = this->head;
+        while (i < index && result->next != nullptr) {
+            result = result->next;
+            i++;
+        }
+        return result->value;
+    }
+
+    container_size length() {
         return this->size;
     }
 };
 
+//template <class T>
+//struct HeapValue{
+//    T value;
+//    HeapValue * leaf1 = nullptr;
+//    HeapValue * leaf2 = nullptr;
+//};
+//
+//template <class T>
+//class Heap{
+//    container_size size = 0;
+//    HeapValue<T> * head = nullptr;
+//
+//public:
+//    Heap<T>()= default;
+//    virtual int compare(T value, T parent) = 0;
+//    void insertElement(T value){
+//        T parent = *head->value;
+//        while (true){
+//            if (compare(value,) == -1){
+//
+//            }
+//        }
+//    }
+//};
 #endif //STDLIB_CONTAINER_H
--- src/main.cpp
+++ src/main.cpp
@@ -2,38 +2,44 @@
 
 #include "Container.h"
 
-void testAdd(){
+void testAdd() {
     ArrayList<int> a;
-    for (int i = 0; i < 100; i++){
+    for (int i = 0; i < 100; i++) {
         if (i < 5 || i > 20) a.add(i);
     }
-    for (container_size i = 0; i < a.length(); i++){
+    for (container_size i = 0; i < a.length(); i++) {
         printf("|%i|,", a.get(i));
     }
     printf("\n");
     for (int i = 5; i <= 20; i++)
         a.insert(i, i);
-    for (container_size i = 0; i < a.length(); i++){
+    for (container_size i = 0; i < a.length(); i++) {
         printf("|%i|,", a.get(i));
     }
 }
 
-void testRemove(){
+void testRemove() {
     ArrayList<int> a;
     a.add(5);
     a.add(6);
     a.add(7);
-    for (container_size i = 0; i < a.length(); i++){
+    for (container_size i = 0; i < a.length(); i++) {
         printf("|%i|,", a.get(i));
     }
     printf("\n");
-    printf("%i\n", a.remove(-5));
-    for (container_size i = 0; i < a.length(); i++){
+    printf("%i\n", a.remove(3));
+    for (container_size i = 0; i < a.length(); i++) {
         printf("|%i|,", a.get(i));
     }
 }
 
 int main() {
-    testRemove();
+    LinkedList_s<int> val;
+    val.add(5);
+    val.add(7);
+    val.add(10);
+    for (int i = 0; i < val.length(); i++) {
+        printf("Value: %i \n", val.get(i));
+    }
     return 0;
 }
